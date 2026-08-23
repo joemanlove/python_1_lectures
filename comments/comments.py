bedb1a4c52d1b9abfe0a9de249e1a717b5714c8e
Joe Manlove <33290590+joemanlove@users.noreply.github.com>
2026-08-23 11:53:07 -0700

    Added "Comments"

---
+++ comments/comments.py
@@ -0,0 +1,11 @@
+"""
+This is a brief introduction to docstrings and comments written by Joe.
+
+This is an introduction to using docstrings and comments for Joe's computer science classes.
+Please follow the formatting here and the advice given in the video.
+For more information and further reading see PEP 8 and PEP 257.
+"""
+
+# Demonstrate the code is working by printing something.
+# Call the print function on the string "hello".
+print("hello")
